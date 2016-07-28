f17f17aa07b347621357e07bfaf44e3859a8a9cd
QuietusPlus <quietus.plus@gmail.com>
2016-07-28 09:30:34 +0200

    Initial commit: Code and documentation

---
+++ Get-SubString.ps1
@@ -0,0 +1,103 @@
+<#
+    The MIT License (MIT)
+
+    Copyright (c) 2016 QuietusPlus
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+#>
+
+function Get-SubString {
+    <#
+    .SYNOPSIS
+        Get -InputObject and return everything between -From and -Until (or until -EmptyLine).
+
+    .DESCRIPTION
+        Get -InputObject and return everything between -From and -Until (or until -EmptyLine).
+
+    .EXAMPLE
+        C:\PS> Get-SubString -InputObject (Get-Help) -From 'Short Description'
+
+    .EXAMPLE
+        C:\PS> Get-SubString -InputObject (Get-Help) -From 'Short Description' -EmptyLine
+
+    .EXAMPLE
+        C:\PS> Get-SubString -InputObject (Get-Help) -From 'Short Description' -Until 'elements'
+
+    .EXAMPLE
+        C:\PS> Get-SubString -InputObject (Get-Help) -Until 'elements'
+
+    .LINK
+        https://quietusplus.github.io/Get-SubString
+
+    .LINK
+        https://github.com/QuietusPlus/Get-SubString
+    #>
+
+    [CmdletBinding()]
+    [OutputType([string])]
+
+    param(
+        # Object to be processed.
+        [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName='From')]
+        [Parameter(Mandatory=$true, ParameterSetName='Until')]
+        [Parameter(Mandatory=$true, ParameterSetName='FromUntil')]
+        [Parameter(Mandatory=$true, ParameterSetName='EmptyLine')]
+        $InputObject,
+
+        # Start capturing at the specified string.
+        [Parameter(Position=1, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName='From')]
+        [Parameter(Mandatory=$true, ParameterSetName='FromUntil')]
+        [Parameter(Mandatory=$true, ParameterSetName='EmptyLine')]
+        [string]
+        $From,
+
+        # Stop capturing once the specified string has been reached.
+        [Parameter(Position=2, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName='Until')]
+        [Parameter(Mandatory=$true, ParameterSetName='FromUntil')]
+        [string]
+        $Until,
+
+        # Stop capturing once a blank line has been reached.
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory=$true, ParameterSetName='EmptyLine')]
+        [switch]
+        $EmptyLine
+    )
+
+    begin {
+        # Check if -From has been specified, if so use it
+        if ($From) { $fromCondition = "(?:$From)"
+        # Otherwise start from top of -InputObject
+        } else { $fromCondition = '' }
+
+        # Check if -EmptyLine has been specified, if so process until empty line has been reached
+        if ($EmptyLine) { $untilCondition = '(?:\r*\n){2}'
+        # Otherwise use -Until if it has been specified
+        } elseif ($Until -notlike $null) { $untilCondition = "(?:$Until)"
+        # Otherwise return everything till end of -InputObject
+        } else { $untilCondition = '(?:.+)' }
+    }
+
+    process {
+        [regex]::Match($($InputObject | Out-String), "(?s)$fromCondition(.*?)$untilCondition", @('MultiLine', 'Ignorecase')).Value
+    }
+}
---
+++ README.md
@@ -0,0 +1,103 @@
+# Description
+
+Get -InputObject and return everything between -From and -Until (or until -EmptyLine).
+
+The script currently supports the following operations:
+
+|Example Title|Input|From|Until|
+|-- |-- |-- |-- |-- |
+| Get -InputObject -From until last line| `-InputObject` | `-From` | Last line |
+| Get -InputObject -From until empty line | `-InputObject` | `-From` | Empty line |
+| Get -InputObject -From -Until | `-InputObject` | `-From` | `-Until` |
+| Get -InputObject -Until | `-InputObject` | First line | `-Until` |
+
+# Examples
+
+## Base Data
+
+```PowerShell
+$InputString = Get-Help | Out-String -Stream | Select-Object -First 15
+```
+
+```
+TOPIC
+    Windows PowerShell Help System
+
+SHORT DESCRIPTION
+    Displays help about Windows PowerShell cmdlets and concepts.
+
+LONG DESCRIPTION
+    Windows PowerShell Help describes Windows PowerShell cmdlets,
+    functions, scripts, and modules, and explains concepts, including
+    the elements of the Windows PowerShell language.
+
+    Windows PowerShell does not include help files, but you can read the
+    help topics online, or use the Update-Help cmdlet to download help files
+    to your computer and then use the Get-Help cmdlet to display the help
+```
+
+## Get -InputObject -From until last line
+
+```PowerShell
+Get-SubString -InputObject $InputString -From 'Short Description'
+```
+
+```
+SHORT DESCRIPTION
+    Displays help about Windows PowerShell cmdlets and concepts.
+
+LONG DESCRIPTION
+    Windows PowerShell Help describes Windows PowerShell cmdlets,
+    functions, scripts, and modules, and explains concepts, including
+    the elements of the Windows PowerShell language.
+
+    Windows PowerShell does not include help files, but you can read the
+    help topics online, or use the Update-Help cmdlet to download help files
+    to your computer and then use the Get-Help cmdlet to display the help
+```
+
+## Get -InputObject -From until empty line
+
+```PowerShell
+Get-SubString -InputObject $InputString -From 'Short Description' -EmptyLine
+```
+
+```
+SHORT DESCRIPTION
+    Displays help about Windows PowerShell cmdlets and concepts.
+```
+
+## Get -InputObject -From -Until
+
+```PowerShell
+Get-SubString -InputObject $InputString -From 'Short Description' -Until 'elements'
+```
+
+```
+SHORT DESCRIPTION
+    Displays help about Windows PowerShell cmdlets and concepts.
+
+LONG DESCRIPTION
+    Windows PowerShell Help describes Windows PowerShell cmdlets,
+    functions, scripts, and modules, and explains concepts, including
+    the elements
+```
+
+## Get -InputObject -Until
+
+```PowerShell
+Get-SubString -InputObject $InputString -Until 'elements'
+```
+
+```
+TOPIC
+    Windows PowerShell Help System
+
+SHORT DESCRIPTION
+    Displays help about Windows PowerShell cmdlets and concepts.
+
+LONG DESCRIPTION
+    Windows PowerShell Help describes Windows PowerShell cmdlets,
+    functions, scripts, and modules, and explains concepts, including
+    the elements
+```
